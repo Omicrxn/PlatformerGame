62fbbd28ded1902f47c54995cd80a2dc612001a3
alexavila0128@gmail.com <alexavila0128@gmail.com>
2020-12-06 22:00:08 +0100

    Checkpoint bug fixed

--- Game/Source/Scene.cpp
+++ Game/Source/Scene.cpp
@@ -78,15 +78,11 @@ bool Scene::Start()
 	coin = (Coin*)app->entityman->CreateEntity(EntityType::ITEM_COIN);
 	heart = (Heart*)app->entityman->CreateEntity(EntityType::ITEM_HEART);
 	
-
 	checkpoint1 = (Checkpoint*)app->entityman->CreateEntity(EntityType::CHECKPOINT);
-	checkpoint1->position = { 1328,944 };
 	checkpoint2 = (Checkpoint*)app->entityman->CreateEntity(EntityType::CHECKPOINT);
-	checkpoint2->position = { 1936,224 };
 	checkpoint3 = (Checkpoint*)app->entityman->CreateEntity(EntityType::CHECKPOINT);
-	checkpoint2->position = { 944, 1328 };
-
-
+	checkpoint2->position = { 1328,944 };
+	checkpoint3->position = { 1936,224 };
 
 
 	// Load music
